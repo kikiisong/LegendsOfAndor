7b1fbfca51b8115211c47fa746fcd9a369cc4f12
0LoveToLearn0 <yaakov@coursera-something.com>
2019-09-23 14:05:59 -0400

    photon

---
+++ _modules/Networking/Assets/Scripts/Networking/NetworkUtils.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Networking
+{
+    public static class NetworkUtils
+    {
+        /// <summary>
+        /// Returns a serialized version of the message as a string
+        /// </summary>
+        public static string ConvertToString<M>(M networkMessage)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            formatter.Serialize(ms, networkMessage);
+
+            return Encoding.ASCII.GetString(ms.GetBuffer());
+        }
+
+        public static O ConvertTo<O>(string serializedMessage)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream receive = new MemoryStream(Encoding.ASCII.GetBytes(serializedMessage));
+            O message = (O)formatter.Deserialize(receive);
+            return message;
+        }
+
+        public static NetworkMessage ConvertToMessage(string serializedMessage)
+        {
+            return ConvertTo<NetworkMessage>(serializedMessage);
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            Debug.Log(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "didnt work";
+        }
+    }
+}
---
+++ _modules/Networking/Assets/Scripts/Test.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+using System.Text;
+using Networking;
+
+public class Test : MonoBehaviour
+{
+    //byte[] recBuffer = new byte[1024];
+    // Use this for initialization
+    void Start()
+    {
+        /*BinaryFormatter formatter = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream();
+        formatter.Serialize(ms, gameObject);
+
+        string send = Encoding.ASCII.GetString(ms.GetBuffer());
+
+        MemoryStream receive = new MemoryStream(Encoding.ASCII.GetBytes(send));
+        GameObject test = (GameObject) formatter.Deserialize(receive);
+        Debug.Log(test.ToString());*/
+        /*
+        string send = NetworkUtils.ConvertToString(transform);
+        Transform receive = NetworkUtils.ConvertTo<Transform>(send);
+        Debug.Log(receive);
+        */
+
+        string json = JsonUtility.ToJson(gameObject);
+        Debug.Log(json);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
