290e384b2f1e50cde0e0bb52ac0b6cd30c515fae
0LoveToLearn0 <yaakov@coursera-something.com>
2019-09-25 18:23:39 -0400

    gitignore

--- _tests/Photon/Assets/Scripts/TestConnect.cs
+++ _tests/Photon/Assets/Scripts/TestConnect.cs
@@ -10,6 +10,7 @@ public class TestConnect : MonoBehaviourPunCallbacks
     void Start()
     {
         print("Connecting to server.");
+        PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
         PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
         PhotonNetwork.ConnectUsingSettings();
@@ -19,10 +20,18 @@ public class TestConnect : MonoBehaviourPunCallbacks
     {
         print("Connected to server.");
         print(PhotonNetwork.LocalPlayer.NickName);
+
+        if (!PhotonNetwork.InLobby) PhotonNetwork.JoinLobby();
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         print("Disconnected from server for reason : " + cause.ToString());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        print("Joined Lobby: " + PhotonNetwork.CurrentLobby.ToString());
+        print("Version: " + PhotonNetwork.GameVersion);
     }
 }
