1f8bae071ad85fb34440ff0a86b7b93d84067968
0LoveToLearn0 <yaakov@coursera-something.com>
2019-09-27 19:57:28 -0400

    scheduled commit

--- reports/_more/create.py
+++ reports/_more/create.py
@@ -1,6 +1,19 @@
 import os
 import shutil as s
+import datetime
 
-s.copy("./template.md", "./t.md")
+def next_weekday(d, weekday):
+    days_ahead = weekday - d.weekday()
+    if days_ahead <= 0:
+        days_ahead += 7
+    return d + datetime.timedelta(days_ahead)
+
+d = datetime.date(2011, 7, 2)
+next_monday = next_weekday(d, 0)
+name = "./" + next_monday + ".md"
+
+s.copy("./template.md", name)
+
+
 
     
