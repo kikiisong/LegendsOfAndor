9e44bbd0980cdffba2c384c73438afcc9d28c359
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2019-11-20 22:53:37 -0500

    Update scenes
    Still work on the scripts

---
+++ Legends of Andor/Assets/Scripts/Fight/TradeSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TradeSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ToTrade()
+    {
+        SceneManager.LoadScene("TradeScene");
+    }
+
+    public void ToMap()
+    {
+        SceneManager.LoadScene("TradeScene");
+    }
+
+    public void ToFight()
+    {
+       
+    }
+
+
+}
