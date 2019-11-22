9a6d051882007b4011e9366045355ab84c8e2dc3
0LoveToLearn0 <yaakov@coursera-something.com>
2019-11-21 21:35:15 -0500

    g

--- Legends of Andor/Assets/Scripts/Fight/TradeSwitch.cs
+++ Legends of Andor/Assets/Scripts/Fight/TradeSwitch.cs
@@ -8,6 +8,7 @@ public class TradeSwitch : MonoBehaviour
     // Start is called before the first frame update
     public void ToTrade()
     {
+        print("a");
         SceneManager.LoadScene("TradeScene");
     }
 
