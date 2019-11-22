93f53f151e061dfbc9b80aa6cea7f843175428b4
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2019-11-22 01:09:48 -0500

    ；

--- Legends of Andor/Assets/Scripts/Fight/TradeSwitch.cs
+++ Legends of Andor/Assets/Scripts/Fight/TradeSwitch.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class TradeSwitch : MonoBehaviour
 {
+
+    private void Start()
+    {
+        print("hey");
+    }
     // Start is called before the first frame update
     public void ToTrade()
     {
         SceneManager.LoadScene("TradeScene");
+        print("hey2");
     }
 
     public void ToMap()
