7348fc114846d79a7c4f20a490ca101c4245529b
chellycheng <hehuimin.cheng@mail.mcgill.ca>
2019-11-23 23:10:15 -0500

    123

---
+++ Legends of Andor/Assets/Scripts/Distribute/AddAndMinus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddAndMinus : MonoBehaviour
+{
+    Text w;
+    Text c;
+    Button plusWill;
+    Button plusStrength;
+    Button minusWill;
+    Button minusStrength;
+    private int currentscore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        w = GetComponentInChildren<Text>();
+        c = GetComponentInChildren<Text>();
+        w.text = ""+currentscore;
+        c.text = ""+currentscore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
