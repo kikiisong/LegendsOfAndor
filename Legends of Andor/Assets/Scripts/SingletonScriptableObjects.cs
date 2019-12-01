d4683a6ee0d0a07c4f669d3f9f739b292f85c757
0LoveToLearn0 <yaakov@coursera-something.com>
2019-11-30 23:57:39 -0500

    f

---
+++ Legends of Andor/Assets/Scripts/SingletonScriptableObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
+{
+    private static T instance = null;
+
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = Resources.FindObjectsOfTypeAll<T>()[0];
+            }
+            return instance;
+        }
+    }
+}
