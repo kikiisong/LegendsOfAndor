645071e06a4438a4151a5513a91a1367d56991f3
0LoveToLearn0 <yaakov@coursera-something.com>
2019-12-01 11:44:51 -0500

    Update popUp.cs

--- Legends of Andor/Assets/Scripts/Trade/popUp.cs
+++ Legends of Andor/Assets/Scripts/Trade/popUp.cs
@@ -8,23 +8,29 @@ public class popUp : MonoBehaviour
 
 	private bool showPopUp = false;
 
+    private int coin;
+
 	public void testA()
 	{
        
-       showPopUp = true;
+        showPopUp = true;
+        Init();
     
         // put your if statement here and set showPopUp to true if the condition is true
         // put an else statement to set the bolean to false if it is not true
-
     }
 
-	void OnGUI()
+    void Init()
+    {
+        coin = Random.Range(1, 4);
+    }
+
+    void OnGUI()
 	{
 		if (showPopUp)
 		{
-			GUI.Window(0, new Rect((Screen.width / 2) - 150, (Screen.height / 2) - 75
-				   , 300, 250), ShowGUI, "Buy an Item");
-
+			GUILayout.Window(0, new Rect((Screen.width / 2) - 150, (Screen.height / 2) - 75
+				   , 300, 250), ShowGUI, "Buy an Item"); 
 		}
 	}
 
@@ -32,16 +38,22 @@ public class popUp : MonoBehaviour
 	{
         // You may put a label to show a message to the player
         //TODO: coin
-        int coin = 2;
-		GUI.Label(new Rect(65, 40, 200, 50), "Buying this object will cost u " +coin+ " coins. Do you confirm?");
-
-		// You may put a button to close the pop up too
-
-		if (GUI.Button(new Rect(50, 150, 75, 30), "OK"))
+        GUILayout.BeginHorizontal();
+		GUILayout.Label("Buying this object will cost u " + coin + " coins. Do you confirm?");
+        GUILayout.EndHorizontal();
+        
+        GUILayout.FlexibleSpace();
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Confirm"))
 		{
 			showPopUp = false;
 			// you may put other code to run according to your game too
 		}
-
+        else if (GUILayout.Button("Cancel"))
+        {
+            showPopUp = false;
+            // you may put other code to run according to your game too
+        }
+        GUILayout.EndHorizontal();
 	}
 }
