7a7b3d7e1d46b26fb72114137bafd6c21f948b0a
0LoveToLearn0 <yaakov@coursera-something.com>
2020-01-31 14:43:01 -0500

    scheduled commit

--- reports/_more/scheduled-commit.bat
+++ reports/_more/scheduled-commit.bat
@@ -1,4 +1,4 @@
-cd C:\Users\bpech\OneDrive\Desktop\Software Project\f2019-hexanome-12\reports\_more
+cd C:\Users\bpech\OneDrive\Desktop\School\f2019-hexanome-12\reports\_more
 python ./create.py %*
 git add .
 git add ..
