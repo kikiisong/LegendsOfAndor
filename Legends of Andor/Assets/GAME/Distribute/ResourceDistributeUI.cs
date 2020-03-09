a621f43784fde0d9a9d212136634a7456b17167a
0LoveToLearn0 <yaakov@coursera-something.com>
2020-03-09 16:57:57 -0400

    Maybe?

--- Legends of Andor/Assets/GAME/Distribute/ResourceDistributeUI.cs
+++ Legends of Andor/Assets/GAME/Distribute/ResourceDistributeUI.cs
@@ -2,32 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Photon.Pun;
 
-public class ResourceDistributeUI : MonoBehaviour
+public class ResourceDistributeUI : MonoBehaviourPun
 {
-    public TextMeshProUGUI amountView;
+    public TextMeshProUGUI text;
     public int amount;
+
+    public Resource resource;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Display();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Display()
     {
-        
+        text.text = amount.ToString();
     }
 
     public void Click_Plus()
     {
+        if (photonView.IsMine)
+        {
+            photonView.RPC("Plus", RpcTarget.All);
+        }
+    }
 
+    [PunRPC]
+    public void Plus()
+    {
+        resource.Take(ref amount);
+        Display();
     }
 
     public void Click_Minus()
     {
+        if (photonView.IsMine)
+        {
+            photonView.RPC("Minus", RpcTarget.All);
+        }
+    }
 
+    [PunRPC]
+    public void Minus()
+    {
+        resource.GiveBack(ref amount);
+        Display();
     }
 }
