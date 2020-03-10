cd8624d4febe7bf6ed7907f4290e8ecaa39b0d31
0LoveToLearn0 <yaakov@coursera-something.com>
2020-03-09 20:06:26 -0400

    requestOwner

---
+++ Legends of Andor/Assets/Scripts/_other/OwnershipTransfer.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnershipTransfer : MonoBehaviourPun, IPunOwnershipCallbacks
+{
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        print("Ownership Transfered");
+    }
+}
