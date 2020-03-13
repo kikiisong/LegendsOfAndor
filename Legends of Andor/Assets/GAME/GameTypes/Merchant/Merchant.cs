3f69e15141319b6c58d57fe30a6bb8dbb313238b
Doreen He <doreen.he9884@gmail.com>
2020-03-13 16:45:21 -0400

    removed unnecessary

--- Legends of Andor/Assets/GAME/GameTypes/Merchant/Merchant.cs
+++ Legends of Andor/Assets/GAME/GameTypes/Merchant/Merchant.cs
@@ -32,54 +32,7 @@ public class Merchant : MonoBehaviour
     }
 
 
-    public void openMenu(Hero h)
-    {
-        print("Working");
-        //SceneManager.LoadSceneAsync("MerchantScene", LoadSceneMode.Additive);
-        
-        //buySP(h, amt)
-        //buyWineSkin()
-        //buyShield()
-        //buyFelcon()
-        //buyBow()
-        //buyHelm()
-        //buyTelescope()
-
-    }
-
-
-
-    void buySP(Hero h, int amt)
-    {
-
-        //dwarf hero type
-        if (h.type == Hero.Type.DWARF && this.isDawrf == true)
-        {
-            if (h.data.gold >= amt)
-            {
-                h.data.gold -= amt;
-                h.data.SP += amt;
-            }
-            else
-            {
-                //pop not enough gold
-            }
-        }
-        //other hero type
-        else
-        {
-            if (h.data.gold >= 2 * amt)
-            {
-                h.data.gold -= (2 * amt);
-                h.data.SP += amt;
-            }
-            else
-            {
-                //pop not enough gold
-            }
-        }
-
-    }
+    
 
     private void OnMouseDown()
     {
