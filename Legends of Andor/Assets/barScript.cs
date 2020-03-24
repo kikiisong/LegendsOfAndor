0b37d9bb867a6edd8a7416e1f079654c8d2442b1
Doreen He <doreen.he9884@gmail.com>
2020-03-23 22:42:30 -0400

    layout

---
+++ Legends of Andor/Assets/barScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class barScript : MonoBehaviour
+{
+
+    public Slider slider;
+    private Hero hero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hero = (Hero)PhotonNetwork.LocalPlayer.CustomProperties[K.Player.hero];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = hero.data.WP;
+    }
+
+    
+}
