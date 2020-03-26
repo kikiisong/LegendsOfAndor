e8ebb975a4577f6a2fd79f0a5b4501139c68d8c4
Doreen He <doreen.he9884@gmail.com>
2020-03-25 21:20:58 -0400

    Layout game map, panel animation, sync hero stats

--- Legends of Andor/Assets/Scripts/MainScenes/GameMap/heroPanelUI.cs
+++ Legends of Andor/Assets/Scripts/MainScenes/GameMap/heroPanelUI.cs
@@ -12,14 +12,14 @@ public class heroPanelUI : MonoBehaviour
     
     public Slider wp;
     public Slider sp;
+    
 
     public TextMeshProUGUI gold;
     public TextMeshProUGUI wpNum;
     public TextMeshProUGUI spNum;
 
-
-
-
+    public Image icon;
+    
 
     Hero hero
     {
@@ -33,21 +33,24 @@ public class heroPanelUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //icon = GetComponent<Image>();
+        icon.sprite = hero.ui.GetSprite();
     }
 
-    // Update is called once per frame
     
+
+    // Update is called once per frame
     void Update()
     {
 
         wp.value = hero.data.WP;
-
+        sp.value = hero.data.SP;
         wpNum.text = "WILLPOWER:  " + hero.data.WP.ToString();
         spNum.text = "STRENGTH:  " + hero.data.SP.ToString();
-
-        sp.value = hero.data.SP;
         gold.text = hero.data.gold.ToString();
 
     }
+
+    
 }
