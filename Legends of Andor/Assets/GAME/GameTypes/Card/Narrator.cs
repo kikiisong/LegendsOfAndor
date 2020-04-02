6b3d17b5819f4f75044a8e9027581358da650c7b
MattGuo <jinhao.guo@mail.mcgill.ca>
2020-04-02 00:48:20 -0400

    Updated Narrator and Event Card
    Narrator can move now, add a task icon, add a event card icon

--- Legends of Andor/Assets/GAME/GameTypes/Card/Narrator.cs
+++ Legends of Andor/Assets/GAME/GameTypes/Card/Narrator.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 namespace Card
 {
-    public class Narrator : MonoBehaviour
+    public class Narrator : MonoBehaviourPun, TurnManager.IOnSunrise
     {
         public static Narrator Instance;
+        public List<Transform> narratorPositions;
+        public int currentLoc;
 
         private void Awake()
         {
@@ -19,20 +23,68 @@ namespace Card
         // Start is called before the first frame update
         void Start()
         {
+            TurnManager.Register(this);
             //TODO: move from region 80 to A
-            LegendCard.Cards[LegendCard.Letter.A].Event();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
+            //  LegendCard.Cards[LegendCard.Letter.A].Event();
 
         }
 
         //call once, affects everyone
-        public static void Advance()
+        public void Advance()
         {
-            //Instance.Move
+            currentLoc = currentLoc + 1;
+
+            // need to decide if we win or lose
+            if (currentLoc >= 14)
+            {
+                print("you have reached the end of the game");
+                return;
+            }
+            else
+            {
+                transform.position = narratorPositions[currentLoc].position;
+            }
+
+            handLegendCard();
+            releaseNewRventCard();
         }
+
+        public void releaseNewRventCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void handLegendCard()
+        {
+            if(currentLoc == 2)
+            {
+                print("time to release Legend C");
+            }
+            else if(currentLoc == 6)
+            {
+                print("time to release Legend G");
+            }
+            else if(currentLoc == 13)
+            {
+                print("time to release Legend N");
+            }
+        }
+
+
+        public void MoveToLetter(int letter)
+        {
+            transform.position = narratorPositions[letter].position;
+        }
+
+        public void OnSunrise()
+        {
+            print("the sunrise has been called");
+
+            Advance();
+
+            // new event card will show up;
+        }
+
+
     }
 }
