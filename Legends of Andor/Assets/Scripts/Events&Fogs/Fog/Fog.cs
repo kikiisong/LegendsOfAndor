fc363e1d4cafedf4cd9df67064798b485e0ffe2d
0LoveToLearn0 <yaakov@coursera-something.com>
2020-04-08 10:19:31 -0400

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/COMP361/f2019-hexanome-12""""
    This reverts commit 3b210bf3205fcea273b8634d63a019279dbd256f.

--- Legends of Andor/Assets/Scripts/Events&Fogs/Fog/Fog.cs
+++ Legends of Andor/Assets/Scripts/Events&Fogs/Fog/Fog.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public abstract class Fog : MonoBehaviourPun
+public class Fog : MonoBehaviourPun
 {
     public int region;
+    public int type;
+    public EventCardController myEvents;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameGraph.Instance.PlaceAt(gameObject, region);
     }
+
 
     // Update is called once per frame
     void Update()
@@ -19,5 +23,44 @@ public abstract class Fog : MonoBehaviourPun
         
     }
 
-    public abstract void uncover();
+
+    public void uncover()
+    {
+        if(type == 1)//SP+1
+        {
+            Hero hero = (Hero)PhotonNetwork.LocalPlayer.GetHero();
+            hero.data.SP += 1;
+        }else if(type == 2)//WP+2
+        {
+            Hero hero = (Hero)PhotonNetwork.LocalPlayer.GetHero();
+            hero.data.WP += 2;
+        }
+        else if (type == 3)//WP+2
+        {
+            Hero hero = (Hero)PhotonNetwork.LocalPlayer.GetHero();
+            hero.data.WP += 3;
+        }
+        else if (type == 4)//Gold
+        {
+            Hero hero = (Hero)PhotonNetwork.LocalPlayer.GetHero();
+            hero.data.gold += 1;
+        }
+        else if (type == 5)//Event
+        {
+            myEvents.flipped();
+        }
+        else if (type == 6)//Wineskine
+        {
+            
+        }
+        else if (type == 7)//Witch
+        {
+            PhotonNetwork.Instantiate("Witch", transform.position, transform.rotation);
+        }
+        else if(type ==8)//Gor
+        {
+            PhotonNetwork.Instantiate("Gor", transform.position, transform.rotation);
+        }
+    }
+
 }
