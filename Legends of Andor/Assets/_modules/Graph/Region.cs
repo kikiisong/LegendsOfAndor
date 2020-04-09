c76b3fe161e3e4ea137c23aefde54b9cf1654bfa
Ren <ter.renata@gmail.com>
2020-04-09 12:12:19 -0400

    Drop Item

--- Legends of Andor/Assets/_modules/Graph/Region.cs
+++ Legends of Andor/Assets/_modules/Graph/Region.cs
@@ -6,7 +6,31 @@ public class Region : Vertex
 {
     public string regionName;
     public Vector3 position;
+    public Data data;
+    //Easy to serialize and sync accross all players
+    [System.Serializable]
+    public struct Data
+    {
+        public int numOfItems;
+        public int gold;
 
+        //small item
+        public int numWineskin;
+        public int brew;
+
+        //number of herb can be used in two way
+        public int herb;
+
+        //big item
+        public int sheild;
+        public int helm;
+
+        //2 means full, 1 means half, 0 means nothing
+        public int bow;
+        public int falcon;
+
+
+    }
     public Region Init(int label, Vector3 vector)
     {
         base.Init(label);
