e080e32b5d5beed4e54247d83eac1760c410c45b
0LoveToLearn0 <yaakov@coursera-something.com>
2020-04-12 18:25:35 -0400

    prince move

---
+++ Legends of Andor/Assets/GAME/GameTypes/Hero/Prince.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prince : MonoBehaviour
+{
+    public static Prince Instance
+    {
+        get
+        {
+            var prince = FindObjectOfType<Prince>();
+            if(prince != null) Debug.Log(prince.name + " " + prince.transform.position);
+            return prince;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
