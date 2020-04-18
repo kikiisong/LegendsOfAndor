324a86137dd252ef854199f804bb3f862c74ff83
kikiisong <55461623+kikiisong@users.noreply.github.com>
2020-04-18 19:37:28 -0400

    Rune stone prefabs done feel free to use
    Blue prefab is the parent, any change to it, yellow and green will inherit

---
+++ Legends of Andor/Assets/Scripts/Events&Fogs/RuneStone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneStone : MonoBehaviour
+{
+    public Color myColor;
+
+    public enum Color
+    {
+        BLUE, GREEN, YELLOW
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
