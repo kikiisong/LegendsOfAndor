29105d30e21ac3e5417ddee16273e9f306b422db
kikiisong <55461623+kikiisong@users.noreply.github.com>
2020-04-28 19:13:20 -0400

    ui

--- Legends of Andor/Assets/Scripts/MainScenes/GameMap/UI/CheckMap.cs
+++ Legends of Andor/Assets/Scripts/MainScenes/GameMap/UI/CheckMap.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
 
 public class CheckMap : MonoBehaviour
 {
@@ -22,6 +24,19 @@ public class CheckMap : MonoBehaviour
     {
         regionInfoPanel.SetActive(true);
         string s = "";
+
+        Player[] players = PhotonNetwork.PlayerList;
+        s += "Hero on Region: ";
+        for (int i = 0; i < players.Length; i++)
+        {
+            Hero hero = (Hero)players[i].GetHero();
+            s += hero.GetCurrentRegion().label + "("+ hero.type +")";
+        }
+
+        s += "\r\n";
+
+
+
         Well[] wells = GameObject.FindObjectsOfType(typeof(Well)) as Well[];
         if(wells.Length>0)
         {
@@ -35,7 +50,46 @@ public class CheckMap : MonoBehaviour
             s += "\r\n";
         }
 
-        Fog[] fogs = GameObject.FindObjectsOfType(typeof(Fog)) as Fog[]; 
+        Fog[] fogs = GameObject.FindObjectsOfType(typeof(Fog)) as Fog[];
+        if (fogs.Length > 0)
+        {
+            s += "Fog on Region: ";
+
+            foreach (Fog f in fogs)
+            {
+                s += f.region + "  ";
+            }
+
+            s += "\r\n";
+        }
+
+
+        Witch[] witch = GameObject.FindObjectsOfType(typeof(Witch)) as Witch[];
+        if (witch.Length > 0)
+        {
+            s += "Witch on Region: ";
+
+            foreach (Witch wi in witch)
+            {
+                s += wi.region + "  ";
+            }
+
+            s += "\r\n";
+        }
+
+        Herb[] herb = GameObject.FindObjectsOfType(typeof(Herb)) as Herb[];
+        if (herb.Length > 0)
+        {
+            s += "Herb on Region: ";
+
+            foreach (Herb h in herb)
+            {
+                s += h.getRegion() + "  ";
+            }
+
+            s += "\r\n";
+        }
+
 
         Debug.Log(s);
     }
