68d6c3386ba531a5eca6b1b221391ec2518c94a1
kikiisong <55461623+kikiisong@users.noreply.github.com>
2020-04-28 18:44:10 -0400

    ui

---
+++ Legends of Andor/Assets/Scripts/MainScenes/GameMap/UI/CheckMap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMap : MonoBehaviour
+{
+    public GameObject regionInfoPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        regionInfoPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void checkRegionInfo()
+    {
+        regionInfoPanel.SetActive(true);
+        string s = "";
+        Well[] wells = GameObject.FindObjectsOfType(typeof(Well)) as Well[];
+        if(wells.Length>0)
+        {
+            s += "Well on Region: ";
+
+            foreach (Well w in wells)
+            {
+                s += w.region + "  ";
+            }
+
+            s += "\r\n";
+        }
+
+        Fog[] fogs = GameObject.FindObjectsOfType(typeof(Fog)) as Fog[]; 
+
+        Debug.Log(s);
+    }
+}
