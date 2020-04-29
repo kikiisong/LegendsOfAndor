32a89415ad95527f726203e61250081ddc4fe40f
kikiisong <55461623+kikiisong@users.noreply.github.com>
2020-04-28 20:09:19 -0400

    stable button UI done

---
+++ Legends of Andor/Assets/Scripts/MainScenes/GameMap/UI/hideButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideButton : MonoBehaviour
+{
+    public GameObject showButton; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        showButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void hidePanel()
+    {
+        transform.parent.gameObject.SetActive(false);
+        showButton.SetActive(true);
+    }
+}
